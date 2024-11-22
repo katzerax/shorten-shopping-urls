7c79196f2f3be48cd4fd82fe04c2f6ccc653bcc5
K <65287118+katzerax@users.noreply.github.com>
2024-11-22 02:22:12 -0800

    Update shorten-shopping-urls.user.js - comment cleanup

--- shorten-shopping-urls.user.js
+++ shorten-shopping-urls.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         shorten-shopping-urls
 // @namespace    https://github.com/katzerax/shorten-shopping-urls
-// @version      0.4
+// @version      0.5
 // @description  copy shortened urls
 // @author       K
 // @match        https://www.ebay.com/*
@@ -11,37 +11,26 @@
 // @grant        none
 // ==/UserScript==
 
-// Get the current URL of the page
 var currentURL = window.location.href;
 var newURL = "";
-// Create a button
 var button = document.createElement('button');
 button.textContent = 'Copy Shortened URL';
-
 if (currentURL.startsWith('https://www.ebay.com/itm/')){
-    // Insert the button to the right of an element with ID "gh-p-3"
     var ebayelement = document.querySelector('#gh-p-3');
     ebayelement.parentNode.insertBefore(button, ebayelement.nextSibling);
-
     button.addEventListener('click', function() {
-        // Remove everything after the "?" character
         var newURL = currentURL.split('?')[0];
-        // Copy the new URL to the clipboard
         navigator.clipboard.writeText(newURL);
     });
 }
 
 else if (currentURL.includes('https://www.amazon.com/')){
     if (currentURL.indexOf('/dp/') !== -1 || currentURL.indexOf('/gp/product/') !== -1) {
-        // Insert the button to the right of an element with ID "prime-badge"
         var amazonelement = document.querySelector("#nav-belt > div.nav-fill");
         amazonelement.parentNode.insertBefore(button, amazonelement.nextSibling);
-
         button.addEventListener('click', function() {
-            // Remove everything after the "?" character
             var newURL = currentURL.split('ref')[0];
             var newnewURL = newURL.split('?')[0];
-            // Copy the new URL to the clipboard
             navigator.clipboard.writeText(newnewURL);
         });
     }
@@ -50,11 +39,8 @@ else if (currentURL.includes('https://www.amazon.com/')){
 else if (currentURL.includes('https://www.etsy.com/listing/')){
     var etsyelement = document.querySelector("#gnav-header-inner > div.wt-width-full.wt-display-flex-xs.wt-pr-lg-3.wt-flex-lg-1.order-mobile-tablet-2");
     etsyelement.parentNode.insertBefore(button, etsyelement.nextSibling);
-
     button.addEventListener('click', function() {
-        // Remove everything after the "?" character
         var newURL = currentURL.split('?')[0];
-        // Copy the new URL to the clipboard
         navigator.clipboard.writeText(newURL);
     });
 }
